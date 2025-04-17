7e234d98646f43be7e204c5b908c0e4b448475a0
Smaxoi <pengsam15@gmail.com>
2025-04-17 09:25:01 +0800

    Update pt6 reverse int re.c

--- practice/pt6 reverse int cur.c
+++ practice/pt6 reverse int cur.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
 
+int reverse(int num) {
+    static int result = 0;
+
+    if (num == 0) {
+        int temp = result;
+        result = 0; // 重置靜態變數，避免影響下一次呼叫
+        return temp;
+    }
+    
+    result = result * 10 + num % 10;
+    return reverse(num / 10);
+}
+
+int main() {
+    int num = 54321;
+    printf("Reverse of %d = %d\n", num, reverse(num));
+    return 0;
+}
